28d78a526d8a9d5db8fb89e247190db87f545e36
natukikazemizo <kmaeyama@ms4.megaegg.ne.jp>
2018-07-08 19:42:42 +0900

    髪の動きを試作
    ・髪の動きを試作
    ・auto_twostの部品の追加

--- Sedna/src/python/addons/animation_auto_breakdown/auto_twist.py
+++ Sedna/src/python/addons/animation_auto_breakdown/auto_twist.py
@@ -1,3 +1,10 @@
-﻿import bpy
+﻿#!BPY
+# -*- coding: UTF-8 -*-
+# Automatically Twist Pose
+#
+# 2018.07.08 Natukikazemizo
+
+
+import bpy
 
 print("test")
---
+++ Sedna/src/python/addons/animation_auto_breakdown/read_fcurves.py
@@ -0,0 +1,19 @@
+﻿#!BPY
+# -*- coding: UTF-8 -*-
+# Read F-Cureves(Pose Libraly)
+#
+# 2018.07.08 Natukikazemizo
+
+import bpy
+
+def get_pose_library(armature_name):
+	return bpy.data.objects["Armature.Dorothy"].pose_library
+
+def get_pose_markers_list(armature_name):
+	ret_list = []
+	for x in bpy.data.objects["Armature.Dorothy"].pose_library.pose_markers:
+		ret_list.append(x.name)
+	return ret_list
+
+
+print("test")
