05305a05a156e76112003c2a6a59c73caa596a32
natukikazemizo <kmaeyama@ms4.megaegg.ne.jp>
2019-04-14 17:26:36 +0900

    camera_changeからrenderに.py名を変更した。

--- Sedna/src/python/render.py
+++ Sedna/src/python/render.py
@@ -1,8 +1,8 @@
 # !BPY
 # -*- coding: UTF-8 -*-
-# Camera Change
+# Render
 #
-# Change Camera & Frame span
+# Render with Change Camera & Frame span
 #
 # 2019.03.23 Natukikazemizo
 #
@@ -13,12 +13,21 @@ import math
 
 # CONSTANTS
 CURRENT_SCENE = "Root"
+TEST_MODE = True
+test_print_cnt = 0
+TEST_PATH = "//..\\renderResults\\folderTree2\\Test_"
 
 def render(fps, frame_step, frame_start, frame_end, filepath, camera):
+    global test_print_cnt
     bpy.context.scene.render.fps = fps
     bpy.context.scene.frame_step = frame_step
     bpy.context.scene.frame_start = frame_start
-    bpy.context.scene.frame_end = frame_end
+    if TEST_MODE:
+        bpy.context.scene.frame_end = frame_start
+        test_print_cnt += 1
+    else:
+        bpy.context.scene.frame_end = frame_end
+
     bpy.data.scenes[CURRENT_SCENE].render.filepath = filepath
     bpy.context.scene.camera = bpy.data.objects[camera]
     print("#### Render Start #### camera:" + camera + " frame:" + str(frame_start) + \
@@ -33,7 +42,7 @@ print("### start ###")
 
 #bpy.context.window.screen = bpy.data.screens['Render']
 
-render(24, 2, 3001, 3024, "//..\\renderResults\\folderTree2\\", "Camera.001")
+render(24, 2, 3001, 3024, "//..\\renderResults\\folderTree2\\", "Camera.Main")
 
 
 print("### end ###")
