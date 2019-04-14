ea7629bd0e67531f5d5c78a103e64d4470641f44
natukikazemizo <kmaeyama@ms4.megaegg.ne.jp>
2019-04-14 18:19:43 +0900

    render.pyにテストレンダリング機能を実装

--- Sedna/src/python/render.py
+++ Sedna/src/python/render.py
@@ -15,7 +15,7 @@ import math
 CURRENT_SCENE = "Root"
 TEST_MODE = True
 test_print_cnt = 0
-TEST_PATH = "//..\\renderResults\\folderTree2\\Test_"
+TEST_PATH = "//..\\renderResults\\TestPrint\\Test_"
 
 def render(fps, frame_step, frame_start, frame_end, filepath, camera):
     global test_print_cnt
@@ -25,24 +25,49 @@ def render(fps, frame_step, frame_start, frame_end, filepath, camera):
     if TEST_MODE:
         bpy.context.scene.frame_end = frame_start
         test_print_cnt += 1
+        bpy.data.scenes[CURRENT_SCENE].render.filepath = TEST_PATH + \
+            str(test_print_cnt).zfill(4) + "_camera_" + camera + "_" + \
+            str(frame_start).zfill(5) + "_" +str(frame_end).zfill(5)
     else:
         bpy.context.scene.frame_end = frame_end
+        bpy.data.scenes[CURRENT_SCENE].render.filepath = filepath
 
-    bpy.data.scenes[CURRENT_SCENE].render.filepath = filepath
     bpy.context.scene.camera = bpy.data.objects[camera]
-    print("#### Render Start #### camera:" + camera + " frame:" + str(frame_start) + \
+    print("#### Render Start  #### camera:" + camera + " frame:" + str(frame_start) + \
         "-" +str(frame_end))
     bpy.ops.render.render(animation=True)
-    print("#### Render End   #### camera:" + camera + " frame:" + str(frame_start) + \
+
+    if TEST_MODE:
+        bpy.context.scene.frame_start = frame_end
+        bpy.context.scene.frame_end = frame_end
+        test_print_cnt += 1
+        bpy.data.scenes[CURRENT_SCENE].render.filepath = TEST_PATH + \
+            str(test_print_cnt).zfill(4) + "_camera_" + camera + "_" + \
+            str(frame_start).zfill(5) + "_" +str(frame_end).zfill(5)
+        bpy.ops.render.render(animation=True)
+
+
+    print("#### Render End    #### camera:" + camera + " frame:" + str(frame_start) + \
         "-" + str(frame_end))
 
 # main
 
-print("### start ###")
+print("######## START ########")
 
 #bpy.context.window.screen = bpy.data.screens['Render']
 
-render(24, 2, 3001, 3024, "//..\\renderResults\\folderTree2\\", "Camera.Main")
+render(24, 2, 1000, 1084, "//..\\renderResults\\Main\\", "Camera.Main")
+render(24, 2, 1085, 1182, "//..\\renderResults\\Main\\", "Camera.Main")
+render(24, 2, 1183, 1272, "//..\\renderResults\\Main\\", "Camera.Main")
+render(24, 2, 1426, 1498, "//..\\renderResults\\Main\\", "Camera.Main")
+render(24, 2, 1499, 1553, "//..\\renderResults\\Main\\", "Camera.Main")
+render(24, 2, 1554, 1578, "//..\\renderResults\\Main\\", "Camera.Main")
+render(24, 2, 1579, 1676, "//..\\renderResults\\Main\\", "Camera.Main")
+render(24, 2, 1677, 1797, "//..\\renderResults\\Main\\", "Camera.Main")
+render(24, 2, 1798, 1870, "//..\\renderResults\\Main\\", "Camera.Main")
+
+render(24, 2, 4800, 5000, "//..\\renderResults\\Long\\", "Camera.Long")
+
+print("######## END   ########")
 
 
-print("### end ###")
